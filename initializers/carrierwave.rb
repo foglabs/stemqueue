4ca16b19686c2a1260b47668e8dee8e6ca7bfed7
fog <foglaboratories@gmail.com>
2017-05-02 11:34:55 -0400

    remove-creds

--- initializers/carrierwave.rb
+++ initializers/carrierwave.rb
@@ -10,14 +10,14 @@
 # # end
 
 # if !Rails.env.test?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider: 'AWS',                                                       # required
-      aws_access_key_id: ENV['S3_ACCESS'],
-      aws_secret_access_key: ENV['S3_SECRET']                   # required
-    }
+  # CarrierWave.configure do |config|
+  #   config.fog_credentials = {
+  #     provider: 'AWS',                                                       # required
+  #     aws_access_key_id: ENV['S3_ACCESS'],
+  #     aws_secret_access_key: ENV['S3_SECRET']                   # required
+  #   }
 
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = ENV['S3_BUCKET']                                 # required
-  end
+  #   config.fog_provider = 'fog/aws'
+  #   config.fog_directory  = ENV['S3_BUCKET']                                 # required
+  # end
 # end
