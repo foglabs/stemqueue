ec8b3f78351ef926fafe7c6e352c11f4c405911e
fog <foglaboratories@gmail.com>
2017-05-02 11:53:31 -0400

    fog-gem

--- initializers/carrierwave.rb
+++ initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',                                                       # required
     aws_access_key_id: ENV['S3_ACCESS'],
-    aws_secret_access_key: ENV['S3_SECRET']                   # required
+    aws_secret_access_key: ENV['S3_SECRET'],                   # required
+    region: 'us-east-1'
   }
-
-  config.fog_provider = 'fog/aws'
   config.fog_directory  = ENV['S3_BUCKET']                                 # required
 end
 
